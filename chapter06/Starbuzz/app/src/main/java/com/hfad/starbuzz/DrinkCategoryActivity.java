cb58bcf97530b94a6b16288f55ce4a6fcd473ddd
sergeykamyshov <kamishovsergey@gmail.com>
2016-04-19 08:53:43 +0300

    Created project Starbuzz with ListView

---
+++ chapter06/Starbuzz/app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
@@ -0,0 +1,6 @@
+package com.hfad.starbuzz;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class DrinkCategoryActivity extends AppCompatActivity {
+}
---
+++ chapter06/Starbuzz/app/src/main/java/com/hfad/starbuzz/TopLevelActivity.java
@@ -0,0 +1,29 @@
+package com.hfad.starbuzz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class TopLevelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_level);
+
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
+    }
+
+    AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            if (position == 0) {
+                startActivity(new Intent(getApplicationContext(), DrinkCategoryActivity.class));
+            }
+        }
+    };
+}
