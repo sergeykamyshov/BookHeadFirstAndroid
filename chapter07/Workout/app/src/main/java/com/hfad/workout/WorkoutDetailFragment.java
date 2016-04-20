c126c2c8c872ca5a3a552a0ac76074b735573d49
sergeykamyshov <kamishovsergey@gmail.com>
2016-04-20 08:33:01 +0300

    Created project Workout and added 1st layout for fragment

---
+++ chapter07/Workout/app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
@@ -0,0 +1,29 @@
+package com.hfad.workout;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WorkoutDetailFragment extends Fragment {
+
+
+    public WorkoutDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+}
