bf50e26c8e7295c5e530967e0483a89a3a193255
sergeykamyshov <kamishovsergey@gmail.com>
2016-04-24 20:32:54 +0300

    Added fragments and DrawerLayout

--- chapter09/BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ chapter09/BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -1,20 +1,46 @@
 package com.hfad.bitsandpizzas;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.ShareActionProvider;
+
+import com.hfad.bitsandpizzas.fragments.PastaFragment;
+import com.hfad.bitsandpizzas.fragments.PizzaFragment;
+import com.hfad.bitsandpizzas.fragments.StoresFragment;
+import com.hfad.bitsandpizzas.fragments.TopFragment;
 
 public class MainActivity extends Activity {
 
     private ShareActionProvider shareActionProvider;
+    private String[] titles;
+    private ListView drawerList;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        titles = getResources().getStringArray(R.array.titles);
+        drawerList = (ListView) findViewById(R.id.listView);
+        drawerList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles));
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        // при старте загружаем TopActivity
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
     }
 
     @Override
@@ -44,5 +70,47 @@ public class MainActivity extends Activity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+            setActionBarTitle(position);
+            drawerLayout.closeDrawer(drawerList);
+        }
+    }
+
+    private void selectItem(int position) {
+        Fragment fragment;
+        switch (position) {
+            case 1:
+                fragment = new PizzaFragment();
+                break;
+            case 2:
+                fragment = new PastaFragment();
+                break;
+            case 3:
+                fragment = new StoresFragment();
+                break;
+            default:
+                fragment = new TopFragment();
+                break;
+        }
+
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.container_frame, fragment);g
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    private void setActionBarTitle(int position) {
+        String title;
+        if (position == 0) {
+            title = getResources().getString(R.string.app_name);
+        } else {
+            title = titles[position];
+        }
+        getActionBar().setTitle(title);
     }
 }
---
+++ chapter09/BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/fragments/TopFragment.java
@@ -0,0 +1,19 @@
+package com.hfad.bitsandpizzas.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hfad.bitsandpizzas.R;
+
+public class TopFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_top, container, false);
+    }
+}
