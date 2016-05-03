0ec7c88af92a07a6683abf7e030fb589a239956c
sergeykamyshov <kamishovsergey@gmail.com>
2016-05-03 17:23:21 +0300

    Changed output info to Toast from background thread

--- chapter13/Joke/app/src/main/java/com/hfad/joke/DelayedMessageService.java
+++ chapter13/Joke/app/src/main/java/com/hfad/joke/DelayedMessageService.java
@@ -2,16 +2,29 @@ package com.hfad.joke;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.util.Log;
+import android.os.Handler;
+import android.widget.Toast;
 
 public class DelayedMessageService extends IntentService {
 
     public static final String EXTRA_MESSAGE = "message";
 
+    private Handler handler;
+
     public DelayedMessageService() {
         super("DelayedMessageServise");
     }
 
+    // выглядит как грязный хак? "You should not override this method for your IntentService"
+    // метод выполняется в главном потоке
+    // создадим Handler в главном потоке, чтобы можно было обновить интерфейс в фоновом потоке
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        handler = new Handler();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    // выполняется в фоновом потоке
     @Override
     protected void onHandleIntent(Intent intent) {
         synchronized (this) {
@@ -25,7 +38,12 @@ public class DelayedMessageService extends IntentService {
         showText(text);
     }
 
-    private void showText(String text) {
-        Log.v("DelayedMessageServise", text);
+    private void showText(final String text) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
