0b7ab8aeacd0cccf5f80ab4e27035e67643687da
sergeykamyshov <kamishovsergey@gmail.com>
2016-05-03 21:12:27 +0300

    Changed Toast to Notification in Service

--- chapter13/Joke/app/src/main/java/com/hfad/joke/DelayedMessageService.java
+++ chapter13/Joke/app/src/main/java/com/hfad/joke/DelayedMessageService.java
@@ -1,27 +1,19 @@
 package com.hfad.joke;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
-import android.os.Handler;
-import android.widget.Toast;
 
 public class DelayedMessageService extends IntentService {
 
     public static final String EXTRA_MESSAGE = "message";
-
-    private Handler handler;
+    public static final int NOTIFICATION_ID = 5453;
 
     public DelayedMessageService() {
         super("DelayedMessageServise");
-    }
-
-    // выглядит как грязный хак? "You should not override this method for your IntentService"
-    // метод выполняется в главном потоке
-    // создадим Handler в главном потоке, чтобы можно было обновить интерфейс в фоновом потоке
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        handler = new Handler();
-        return super.onStartCommand(intent, flags, startId);
     }
 
     // выполняется в фоновом потоке
@@ -29,7 +21,7 @@ public class DelayedMessageService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         synchronized (this) {
             try {
-                wait(10000);
+                wait(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -39,11 +31,24 @@ public class DelayedMessageService extends IntentService {
     }
 
     private void showText(final String text) {
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
-            }
-        });
+        Intent intent = new Intent(this, MainActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(intent);
+
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getString(R.string.app_name))
+                .setAutoCancel(true)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setContentText(text)
+                .setContentIntent(pendingIntent)
+                .build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID, notification);
     }
 }
