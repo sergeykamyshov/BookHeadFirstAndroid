2c5967a43b89fa39dd65c2ad67913dcc1cf08325
sergeykamyshov <kamishovsergey@gmail.com>
2016-05-03 12:47:54 +0300

    Added CheckBox favorite to DrinkActivity

--- chapter06/Starbuzz/app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
+++ chapter06/Starbuzz/app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
@@ -1,11 +1,15 @@
 package com.hfad.starbuzz;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,7 +31,7 @@ public class DrinkActivity extends AppCompatActivity {
             SQLiteDatabase db = databaseHelper.getReadableDatabase();
 
             Cursor cursor = db.query("DRINK",
-                    new String[]{"NAME", "DESCRIPTION", "IMAGE_RESOUCE_ID"},
+                    new String[]{"NAME", "DESCRIPTION", "IMAGE_RESOUCE_ID", "FAVORITE"},
                     "_id = ?",
                     new String[]{Integer.toString(drinkNo)},
                     null, null, null);
@@ -41,12 +45,34 @@ public class DrinkActivity extends AppCompatActivity {
 
                 ImageView photo = (ImageView) findViewById(R.id.photo);
                 photo.setImageResource(cursor.getInt(2));
+
+                CheckBox favorite = (CheckBox) findViewById(R.id.favorite);
+                boolean isFavorite = cursor.getInt(3) == 1;
+                favorite.setChecked(isFavorite);
             }
 
             cursor.close();
             db.close();
         } catch (SQLiteException e) {
             Toast.makeText(this, "Database unavailable", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onFavoriteClick(View view) {
+        int drinkNo = getIntent().getExtras().getInt(EXTRA_DRINKNO);
+        CheckBox favorite = (CheckBox) view;
+
+        ContentValues values = new ContentValues();
+        values.put("FAVORITE", favorite.isChecked());
+
+        try {
+            SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+            SQLiteDatabase db = starbuzzDatabaseHelper.getWritableDatabase();
+
+            db.update("DRINK", values, "_id = ?", new String[]{Integer.toString(drinkNo)});
+            db.close();
+        } catch (SQLiteException e) {
+            Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT).show();
         }
     }
 }
