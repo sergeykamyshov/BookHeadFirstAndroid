7b19f16d3dc97a3bbcacdc4e59918f16b6dc2bb3
kamyshov.sergey <kamyshov.sergey@otr.ru>
2016-05-04 18:03:37 +0300

    Implemented OdometerService

--- chapter13/Odometer/app/src/main/java/com/hfad/odometer/OdometerService.java
+++ chapter13/Odometer/app/src/main/java/com/hfad/odometer/OdometerService.java
@@ -1,15 +1,85 @@
 package com.hfad.odometer;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 
 public class OdometerService extends Service {
 
+    private static double distanceInMeters;
+    private static Location lastLocation;
+
+    private final IBinder binder = new OdometerBinder();
+
+
+    public class OdometerBinder extends Binder {
+        OdometerBinder getOdometer() {
+            return OdometerBinder.this;
+        }
+    }
+
+    // вызывается при создании службы
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (lastLocation == null) {
+                    lastLocation = location;
+                }
+                distanceInMeters += location.distanceTo(lastLocation);
+                lastLocation = location;
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locationListener);
+    }
+
+    // вызывается при связывании службы с активностью через ServiceConnection
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return binder;
+    }
+
+    public double getMiles() {
+        return distanceInMeters / 1609.344;
     }
 }
