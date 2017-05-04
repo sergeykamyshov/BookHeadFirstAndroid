c26eaebbc7365058c560b443049de8665fad91b1
kamyshov.sergey <kamyshov.sergey@otr.ru>
2017-05-04 19:29:16 +0300

    Revert chapter02 folder

---
+++ chapter02/BeerAdviser/app/src/main/java/com/hdad/beeradviser/FindBeerActivity.java
@@ -0,0 +1,32 @@
+package com.hdad.beeradviser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FindBeerActivity extends AppCompatActivity {
+
+    private BeerExpert expert = new BeerExpert();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+
+    public void onClickFindBeer(View view) {
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+        List<String> recomendedBrands = expert.getBrands(beerType);
+        StringBuilder builder = new StringBuilder();
+        for (String brand : recomendedBrands) {
+            builder.append(brand + "\n");
+        }
+        brands.setText(builder.toString());
+    }
+}
